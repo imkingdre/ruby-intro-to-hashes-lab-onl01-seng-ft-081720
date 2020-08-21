a27482b5736fedd7e0b2dcbb3a16894eff0d967a
Andre King <andreking1031@gmail.com>
2020-08-21 18:10:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,9 @@ def monopoly_with_third_tier
 	    names: => 
 	    {
 	      :reading_railroad => {},
-	      :
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortline => {}
 	      
 	    }
 	  }
